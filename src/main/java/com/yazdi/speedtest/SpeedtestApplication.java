c29dcf1cd6e471494b3dbfdcb5098ea5c6661843
ahmadyz243 <ahmadyazdi243@gmail.com>
2023-08-16 15:39:34 +0330

    start project

---
+++ src/main/java/com/yazdi/speedtest/SpeedtestApplication.java
@@ -0,0 +1,13 @@
+package com.yazdi.speedtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yazdi/speedtest/SpeedtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yazdi.speedtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
